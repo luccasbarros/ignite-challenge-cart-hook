2a4868b2e1c91ea91e237cee29a5416dae259fab
Luccas <luccas.barros@shareprime.com.br>
2021-04-21 11:15:15 -0300

    feat(cart): cart result formatted

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -20,9 +20,11 @@ interface Product {
 const Cart = (): JSX.Element => {
   const { cart, removeProduct, updateProductAmount } = useCart();
 
-  // const cartFormatted = cart.map(product => ({
-  //   // TODO
-  // }))
+  const cartFormatted = cart.map(product => ({
+    ...product,
+    priceFormatted: formatPrice(product.price),
+    subTotal: formatPrice(product.price * product.amount)
+  }))
   // const total =
   //   formatPrice(
   //     cart.reduce((sumTotal, product) => {
@@ -55,7 +57,7 @@ const Cart = (): JSX.Element => {
           </tr>
         </thead>
         <tbody>
-          {cart.map((product: Product) => {
+          {cartFormatted.map((product) => {
             return (
           <tr data-testid="product">
             <td>
@@ -63,7 +65,7 @@ const Cart = (): JSX.Element => {
             </td>
             <td>
               <strong>{product.title}</strong>
-              <span>{formatPrice(product.price)}</span>
+              <span>{product.priceFormatted}</span>
             </td>
             <td>
               <div>
@@ -91,7 +93,7 @@ const Cart = (): JSX.Element => {
               </div>
             </td>
             <td>
-              <strong>{product.price * product.amount}</strong>
+              <strong>{product.subTotal}</strong>
             </td>
             <td>
               <button
